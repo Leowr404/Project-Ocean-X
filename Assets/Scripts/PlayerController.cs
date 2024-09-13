531fc32c0172a7a21d14577085a44938d643191c
Leowr404 <Leonardounity404@gmail.com>
2024-09-13 13:04:44 -0300

    Add Move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int maxHealth;
+    public int currentHealth;
+    public int Velocidade;
+    Vector2 moveInput;
+    private CharacterController characterController;
+    //======Limite De Cenario=========
+    public float xMin = -10f;
+    public float xMax = 10f;
+    public float zMin = -5f;
+    public float zMax = 5f;
+    //================================
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        characterController.Move(move * Velocidade * Time.deltaTime);
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, xMin, xMax);
+        clampedPosition.z = Mathf.Clamp(clampedPosition.z, zMin, zMax);
+        transform.position = clampedPosition;
+    }
+}
